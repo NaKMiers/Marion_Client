b73f181afa63837a63909db5bd28e1071aecc22c
AnK <56575187+NaKMiers@users.noreply.github.com>
2022-04-30 15:19:57 +0700

    add silly dot.

--- src/constants/dataSample.js
+++ src/constants/dataSample.js
@@ -2,16 +2,16 @@ export const notifyContents = [
    {
       image: 'images/notifyAvt1.png',
       title: 'Gucci',
-      description: 'Your order has been confirmed',
+      description: 'Your order has been confirmed.',
    },
    {
       image: 'images/notifyAvt2.png',
       title: 'Gucci',
-      description: 'Your order has been confirmed',
+      description: 'Your order has been confirmed.',
    },
    {
       image: 'images/notifyAvt3.png',
       title: 'Gucci',
-      description: 'Your order has been confirmed',
+      description: 'Your order has been confirmed.',
    },
 ]
